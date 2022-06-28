8d4c0dbf5f6746edb3a94abf7d277628789128b7
NatiG <nati.g5684@gmail.com>
2022-06-28 19:15:56 +0300

    Fix linter

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,2 +1,6 @@
-import { legacy_createStore as createStore } from "redux";
+import { legacy_createStore as createStore } from 'redux';
+import reducer from './reducer';
 
+const store = createStore(reducer);
+
+export default store;
